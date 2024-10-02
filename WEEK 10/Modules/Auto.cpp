7716bab863d910de5438ffbda64b5e8fa21c9b2e
ShubhPundir <shb.pndr@gmail.com>
2024-10-02 16:20:08 +0530

    Week 10

--- WEEK 10/Modules/Auto.cpp
+++ WEEK 10/Modules/Auto.cpp
@@ -11,6 +11,12 @@ int main(){
     const auto *x2 = &x1; // const int *x2 = &x1;
     const auto& i2 = m; // const map<int,string>& i2 = m;
 
-    
+    list<int> li;
+    auto v1 = li; // v1: list<int>
+    auto &v2 = li; // v2: const list<int>&
+
+    float data[5];
+    auto v3 = data; // v3: float*
+    auto& v4 = data; // v4: float(&)[5]
 
 }
