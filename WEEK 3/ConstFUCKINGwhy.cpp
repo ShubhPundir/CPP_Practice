64e7302e90ac22c0cbbf91e2b4b9b8fe3e8c83b1
ShubhPundir <shb.pndr@gmail.com>
2024-10-24 22:23:06 +0530

    Constant Objects Updated

--- WEEK 3/ConstFUCKINGwhy.cpp
+++ WEEK 3/ConstFUCKINGwhy.cpp
@@ -3,27 +3,38 @@ using namespace std;
 
 class myClass{
     int priMem_;
+    mutable int mPriMem_= -4; 
     public: int pubMem_;
     myClass(int pri,int pub) : priMem_(pri), pubMem_(pub){}
 
     int getMember() const {return priMem_;}
     void setMember(int i) {
-        priMem_ = i;
+        priMem_ = i; // not possible even if setMember() is declared with const 
+        // as data members of a constant object cannot be changed
         cout << "Setting nothing fucka" <<endl;
     }
-    void print() const{cout << priMem_ << " "<< pubMem_ <<endl;}
+    
+    void setMemberMutable(int i) const {
+        mPriMem_ = i; // not possible even if setMember() is declared with const 
+        // as data members of a constant object cannot be changed
+        cout << "Setting nothing fucka" <<endl;
+    }
+    void print() const{cout << priMem_ << " "<< pubMem_ << " "<< mPriMem_ << endl;}
 };
 int main()
 {
     myClass a(1,2);
     cout << a.getMember() <<endl;
     a.setMember(4);
+    a.setMemberMutable(4);
     a.print();
+    cout << "-------------------------------------------" << endl;
 
     const myClass b(11,21);
 
     cout << b.getMember() <<endl;
     // b.setMember(4);
+    b.setMemberMutable(4);
     b.print();
     return 0;
 }
