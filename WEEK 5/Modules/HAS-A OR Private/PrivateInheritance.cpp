0d75200980bb33c05a2d5aa3f1f7c6af10285e62
ShubhPundir <shb.pndr@gmail.com>
2024-10-25 14:07:23 +0530

    HAS-A

---
+++ WEEK 5/Modules/HAS-A OR Private/PrivateInheritance.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+/*
+--> Private inheritance means nothing during software design, only during software
+implementation
+
+--> Private inheritance means is-implemented-in-terms of. It is usually infe-
+rior to composition, but it makes sense when a derived class needs access to
+protected base class members or needs to redefine inherited virtual functions
+{ Scott Meyers in Item 32, Effective C++ (3rd. Edition)
+*/
+
+class Engine{
+    public:
+        Engine(int numCylinders): n(numCylinders) {}
+        void start() {cout << "VROOOOMM with "<< n << " Cylinders" << endl;}
+    private:
+        int n;
+};
+
+class Car: private Engine{
+    public:
+        Car() : Engine(8) {}
+        // void start() {e.start();}
+        using Engine::start;
+};
+
+int main(){
+    Car c;
+    c.start();
+} 
--- WEEK 5/Modules/HAS-A OR Private/SimpleComposition.cpp
+++ WEEK 5/Modules/HAS-A OR Private/SimpleComposition.cpp
@@ -1,1 +1,23 @@
 #include <iostream>
+using namespace std;
+
+class Engine{
+    public:
+        Engine(int numCylinders): n(numCylinders) {}
+        void start() {cout << "VROOOOMM with "<< n << " Cylinders" << endl;}
+    private:
+        int n;
+};
+
+class Car{
+    public:
+        Car() : e(8) {}
+        void start() {e.start();}
+    private:
+        Engine e;
+};
+
+int main(){
+    Car c;
+    c.start();
+} 
