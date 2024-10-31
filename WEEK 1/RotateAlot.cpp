738c24359e4dab3c7d6f3ecacc4cafaa06673bd2
ShubhPundir <shb.pndr@gmail.com>
2024-10-31 16:31:30 +0530

    WEEK 0 Changes

--- WEEK 1/RotateAlot.cpp
+++ WEEK 1/RotateAlot.cpp
@@ -8,12 +8,17 @@ int main()
     
     for(int i=0;i<6;i++)
         *(numbers + i) = (i + 1) * 10;
-
-    rotate(numbers,numbers + 5, numbers + 6);
+    // 10 20 30 40 50 60
+    rotate(numbers, numbers + 5, numbers + 6); // rotate(begin, middle, end)
+    // 10 20 30 40 50 {60}
+    // 60 10 20 30 40 50
+    
     rotate(numbers,numbers + 2, numbers + 5);
+    // {60 10} {20 30 40} 50
+    // 60 10 30 40 50 20 
 
     for(int i=0;i<6;i++)
         cout << numbers[i] << " ";
-
+    // 20 30 40 60 10 50
     return 0;
 }
--- WEEK 1/WOW.cpp
+++ WEEK 1/WOW.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 #include <string>
+#include <cstring>
 using namespace std;
 
 int main()
 {
     string message = "Good Morning World";
-    // message.resize(12);
-    char* m;
-    strcpy(m,"Good Morning");
+    message.resize(12);
+    // char* m;
+    // strcpy(m,"Good Morning");
 
-    cout << m;
+    cout << message;
     return 0;
 }
